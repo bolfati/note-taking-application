e3b3a1897bd605dd848695144fca2b8a719f461e
Bij <bijan.olfati@gmail.com>
2023-11-07 18:27:03 -0600

    adding port value for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const uuid = require("./uuid");
 const fs = require("fs");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 //setting up necessary variables 
 
 app.use(express.json());
